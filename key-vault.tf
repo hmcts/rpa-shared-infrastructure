48621456d730d2a08554448e6938904ff9526f95
Alec Doran-Twyford <alec@alectronic.co>
2018-07-16 13:53:13 +0100

    update pg

--- key-vault.tf
+++ key-vault.tf
@@ -6,7 +6,7 @@ module "key_vault" {
   tenant_id = "${var.tenant_id}"
   object_id = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
+  product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
 }
 
 output "vaultName" {

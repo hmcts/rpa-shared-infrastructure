2732d2a46ff448c37eeb21de602341b16fb63b6b
Milan0140 <40658124+Milan0140@users.noreply.github.com>
2019-05-22 14:50:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  version = "1.22.1"
+}
+
 locals {
   tags = "${merge(var.common_tags,
     map("Team Contact", "${var.team_contact}")
@@ -7,6 +11,5 @@ locals {
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = "${var.location}"
-
   tags = "${local.tags}"
 }

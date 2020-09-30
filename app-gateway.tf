4a91c024b8c800caff9bc3cd77409ccbf72e3cc2
benouaer <tech@benouaer.com>
2020-09-30 17:18:51 +0100

    fix build issue

--- app-gateway.tf
+++ app-gateway.tf
@@ -4,7 +4,7 @@ module "appGw" {
   subscription      = "${var.subscription}"
   location          = "${var.location}"
   wafName           = "${var.product}"
-  resourcegroupname = "${azurerm_resource_group.rg.name}"
+  resourcegroupname = "rpa-${var.env}"
   common_tags       = "${var.common_tags}"
 
   # vNet connections

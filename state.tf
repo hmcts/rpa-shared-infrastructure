0b722d0bf633ffbe99185cc73baf295525fda3f5
benouaer <tech@benouaer.com>
2020-09-30 17:48:35 +0100

    fix build issue

--- state.tf
+++ state.tf
@@ -1,16 +1,3 @@
 terraform {
   backend "azurerm" {}
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.25"
-    }
-  }
-}
-
-data "azurerm_subnet" "subnet_a" {
-  name                 = "core-infra-subnet-0-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-  resource_group_name  = "core-infra-${var.env}"
 }

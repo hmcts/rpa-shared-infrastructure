4afff46331ad84d625b5d2c84c88771ab66226fb
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2021-05-05 18:53:14 +0100

    Added in access for dm-store

--- key-vault.tf
+++ key-vault.tf
@@ -8,7 +8,13 @@ module "key_vault" {
   resource_group_name = azurerm_resource_group.rg.name
   product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
   common_tags = local.tags
+  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.dm-shared-identity.principal_id}"]
   create_managed_identity    = true
+}
+
+data "azurerm_user_assigned_identity" "dm-shared-identity" {
+  name                = "dm-store-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 output "vaultName" {
